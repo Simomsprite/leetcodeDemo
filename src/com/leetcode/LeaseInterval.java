afbef4627daa8869417014933e4fd16b44fdaab0
e <307520120@qq.com>
2020-01-13 19:00:11 +0800

    leetcode

--- src/com/leetcode/LeaseInterval.java
+++ src/com/leetcode/LeaseInterval.java
@@ -10,6 +10,8 @@
  */
 package com.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 〈一句话功能简述〉<br> 
  * 〈621. 任务调度器〉
@@ -37,12 +39,21 @@ public class LeaseInterval {
 
     }
 
-    private int leaseInterval(char[] tasks,int num){
-        if (tasks.length <= 1 || num <1) return tasks.length;
+    private int leaseInterval(char[] tasks,int n){
+        if (tasks.length <= 1 || n <1) return tasks.length;
         int[] counts=new int[26];
         for (int i=0;i<tasks.length; i++){
             counts[tasks[i]-'A'] ++;
         }
+        Arrays.sort(counts);
+        int maxCount=counts[25];
+        int retCount=(maxCount-1)*(n+1)+1;
+        int i=24;
+        while (i>=0 && counts[i]==maxCount){
+            retCount++;
+            i--;
+        }
+        return Math.max(retCount,tasks.length);
     }
 }
 
